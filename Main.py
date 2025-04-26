082a195ff4ae5f47167881d9a6c96bce48c58217
Barath <barathofficial25@gmail.com>
2025-04-27 04:58:15 +0530

    Added SQL Connection

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
-from NLP_QW import Get_query
+from NLP_QW import runLogic
 
 query = ''
 
 while query != "Bye":
     query = str(input("Query: "))
 
-    ans = Get_query(content=query)
+    ans = runLogic(content=query)
 
     print(f"Deepseek"+': ',ans.choices[0].message.content)
 
--- NLP_QW.py
+++ NLP_QW.py
@@ -1,10 +1,37 @@
+import sqlite3
 from openai import OpenAI
 
-
+#Deepseek AI R1 API
 client = OpenAI(base_url="https://openrouter.ai/api/v1",
                 api_key="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")
 
-def Get_query(content):
+
+
+
+def fetchData(query):
+    try:
+        #connection to local SQL
+        sqliteConnection = sqlite3.connect('Medical.db')
+        #initiated DB
+        cursor = sqliteConnection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+
+    except sqlite3.Error as error:
+        return error
+
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+
+
+    return result
+
+
+
+#Get Response from Deepseek
+def getQuery(content):
     completion = client.chat.completions.create(
         extra_body={},
         model="deepseek/deepseek-r1-zero:free",
@@ -18,4 +45,13 @@ def Get_query(content):
     return completion
 
 
+def runLogic(content):
+    query = getQuery(content)
+    table = fetchData(query)
+    final_query = "Explain this table "+table
 
+    finalResult = getQuery(final_query)
+    return finalResult
+
+
+fetchData("select * from medicalData;")
